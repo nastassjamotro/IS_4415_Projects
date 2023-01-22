4601edcf87942fae1ec98d4a9a24c881a75b7794
Nastassja Motro <nastassjamotro17@gmail.com>
2023-01-22 16:14:17 -0700

    Create Bicycle.java

---
+++ WeeklySubmissions/Week10/Bicycle.java
@@ -0,0 +1,9 @@
+public class Bicycle extends Vehicle {
+  public void speedUp() {
+    System.out.println("bike goes vroom");
+  }
+  
+  public void slowDown() {
+    System.out.println("bike goes stop");
+  }
+}
