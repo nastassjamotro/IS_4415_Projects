672a46d2ca472918e7a1647f6e2a9764fa1375fa
Nastassja Motro <nastassjamotro17@gmail.com>
2023-01-22 15:18:22 -0700

    Create Part1.java

---
+++ WeeklySubmissions/Week3/Part1.java
@@ -0,0 +1,16 @@
+public class Part1 {
+  public static void stringMethod() {
+    String assignOne = "This is not hard.";
+    //System.out.println(stringSize.lenght());
+    
+    //printing the last word of the string out verticaly
+    System.out.println(assignOne.charAt(12));
+    System.out.println(assignOne.charAt(13));
+    System.out.println(assignOne.charAt(14));
+    System.out.println(assignOne.charAt(15));
+  }
+  
+  public static void main(String args[]) {
+    stringMethod();
+  }
+}
