85b1484536d9920a998a7bcee626382fefe3e411
Nastassja Motro <nastassjamotro17@gmail.com>
2023-01-22 15:30:43 -0700

    Create Wk3Part2.java

---
+++ WeeklySubmissions/Week3/Wk3Part2.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Wk3Part2 {
+  public static void main(String args[]) {
+    // creating int array and sorting it
+    int myArray[] = {9,5,8,3};
+    System.out.println("Integer Array:");
+    Arrays.sort(myArray);
+    System.out.println(Arrays.toString(myArray));
+    System.out.println(" ");
+    
+    //creating string array
+    String myStrings[] = {"Salt", "Lake", "City"};
+    System.out.println("String Array:");
+    System.out.println(myStrings.[0]);
+    System.out.println(myStrings.[1]);
+    System.out.println(myStrings.[2]);
+  }
+}
