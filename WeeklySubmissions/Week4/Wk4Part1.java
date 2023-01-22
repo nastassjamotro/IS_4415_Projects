7b2927e8f1179e9a1cbbe04634d3c27904571393
Nastassja Motro <nastassjamotro17@gmail.com>
2023-01-22 15:34:02 -0700

    Create Wk4Part1.java

---
+++ WeeklySubmissions/Week4/Wk4Part1.java
@@ -0,0 +1,12 @@
+public class Wk4Part1 {
+  public static void main(String args[]) {
+    System.out.print("The student said \"Where is my class?\". \nShe was not sure where to go.\n");
+    
+    float f1 = 11.433445f;
+    float f2 = 121.26345f;
+    float f3 = 3221.5598f;
+    System.out.printf("The student earns $%7.2f\n", f1);
+    System.out.printf("The student earns $%7.2f\n", f2);
+    System.out.printf("The student earns $%7.2f", f3);
+  }
+}
