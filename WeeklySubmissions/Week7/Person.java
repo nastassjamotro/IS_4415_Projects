ae4d733693142f57179277018529100e1b7c447e
Nastassja Motro <nastassjamotro17@gmail.com>
2023-01-22 15:58:43 -0700

    Create Person.java

---
+++ WeeklySubmissions/Week7/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+  private String personName;
+  //private String person1;
+  //private String person2;
+  
+  public Person(String person) {
+    person = personName;
+    //person2 = personName;
+  }
+  
+  public void setNameOfPerson(String person) {
+    personName = person;
+  }
+  
+  public void getNameOfPerson(String person) {
+    personNme = person;
+  }
+}
