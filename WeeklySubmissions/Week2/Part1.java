2453f57a709571590e61c19deff61110e5e33393
Nastassja Motro <nastassjamotro17@gmail.com>
2023-01-22 15:10:31 -0700

    Create Part1.java

---
+++ WeeklySubmissions/Week2/Part1.java
@@ -0,0 +1,18 @@
+public class Part1 {
+  pubic static void main(String args[]) {
+    byte a = 20;
+    short b = 400;
+    int c = 92942;
+    long d = 8888888888888888L;
+    float e = 49383.22f;
+    double f = 2929448203.39484d;
+    char g = 'N';
+    boolean h = true;
+    
+    // make byte function not work by having the number be too big
+    byte tooBig = 29387477292398473;
+    
+    // making short function not work by having number be too small
+    short tooSmall = -3474722383474;
+  }
+}
