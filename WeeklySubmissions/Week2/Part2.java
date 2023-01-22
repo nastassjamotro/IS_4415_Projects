c323ded3c4b485041bfbcc5c326e1be62716ddc4
Nastassja Motro <nastassjamotro17@gmail.com>
2023-01-22 15:12:42 -0700

    Create Part2.java

---
+++ WeeklySubmissions/Week2/Part2.java
@@ -0,0 +1,38 @@
+public class Part2 {
+  public static void main(String args[]) {
+   // adding two integers together
+    int c = 92942;
+    int cTwo = 33583;
+    int cAdd = c + cTwo;
+
+    // adding three longs together
+    long d = 8888888888888888L;
+    long dTwo = 39375752039L;
+    long dThree = 393575L;
+    long dAdd = d + dTwo + dThree;
+
+    // adding two bytes together with += operator
+    byte a = 20;
+    byte aTwo = 40;
+    a += aTwo;
+
+    // dividing two integers together
+    int one = 50;
+    int two = 25;
+    int divide = one/two;
+
+    // using ++ operator on a byte
+    byte plusPlus = 5;
+    plusPlus++;
+
+    // multiply two shorts together
+    short shortieTwo = 20343;
+    short shortie = 203;
+    int bMultiply = shortie * shortieTwo;
+
+    // adding two bytes together
+    byte onesie = 100;
+    byte twosie = 50;
+    int aAdd = onesie + twosie;
+  }
+}
