309f41b7b01f79ea7bc876009625188986028ce7
Nastassja Motro <nastassjamotro17@gmail.com>
2023-01-22 16:03:53 -0700

    Create Animal.java

---
+++ WeeklySubmissions/Week9/Animal.java
@@ -0,0 +1,13 @@
+//each technically in different files but have just chucked them all together here for space
+public class Animal {}
+
+public class Mammal extends Animal{}
+public class Dog extends Mammal{}
+public class Cat extends Mammal{}
+
+public class Fish extends Animal{}
+public class Shark extends Fish{}
+
+public class Insect extends Animal{}
+public class Spider extends Insect{}
+public class Fly extends Insect{}
