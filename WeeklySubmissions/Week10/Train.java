89976e6c356e622700befbdb81bb6f7939fc3f69
Nastassja Motro <nastassjamotro17@gmail.com>
2023-01-22 16:17:53 -0700

    Create Train.java

---
+++ WeeklySubmissions/Week10/Train.java
@@ -0,0 +1,9 @@
+public class Train extends Vehicle{
+  public void speedUp() {
+    System.out.println("train goes vroom");
+  }
+  
+  public void slowDown() {
+    System.out.println("train goes stop");
+  }
+}
