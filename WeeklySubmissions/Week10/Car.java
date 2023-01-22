4b7b16845a951a1524da2e381b7080e6ee34fa6c
Nastassja Motro <nastassjamotro17@gmail.com>
2023-01-22 16:15:12 -0700

    Create Car.java

---
+++ WeeklySubmissions/Week10/Car.java
@@ -0,0 +1,9 @@
+public class Car extends Vehicle {
+  public void speedUp() {
+    System.out.println("car goes vroom");
+  }
+  
+  public void slowDown() {
+    System.out.println("car goes stop");
+  }
+}
